eb959c65e2755521120d1837045dad65b39c9c96
Janzy16 <janiceee456@gmail.com>
2024-03-09 17:54:27 +0800

    edit homepage

--- index.php
+++ index.php
@@ -4,41 +4,46 @@
     <meta charset="utf-8">
     <title>Cup Corner</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 </head>
 <body>
-
-    <div class="container">
-        <div class="navbar">
-            <div class="logo">
-                <a href="index.html"><img src="cupcorner-logo.jpg" width="125px"></a>
+    <div class="header">
+        <div class="container">
+            <div class="navbar">
+                <div class="logo">
+                    <a href="index.html"><img src="cupcorner-logo.jpg" width="125px"></a>
+                </div>
+                <nav>
+                    <ul>
+                        <li><a href="">Home</a></li>
+                        <li><a href="">Menu</a></li>
+                        <li><a href="">About</a></li>
+                        <li><a href="">Contact</a></li>
+                        <li><a href="">Account</a></li>
+                        <li><a href="">Cart</a></li>
+                    </ul>
+                </nav>
             </div>
-
-            <nav>
-                <ul>
-                    <li><a href="">Home</a></li>
-                    <li><a href="">Menu</a></li>
-                    <li><a href="">About</a></li>
-                    <li><a href="">Contact</a></li>
-                    <li><a href="">Account</a></li>
-                    <li><a href="">Cart</a></li>
-                </ul>
-            </nav>
         </div>
+    </div>
+    <div class="container">
         <div class="row">
-            <div= class="col-2">
+            <div class="col-2">
                 <h1>Embrace the Aroma: <br>Elevate Your Coffee Experience Today!</h1>
                 <p>Savoring a perfect cup of coffee isn't just about that initial burst of flavor,<br>it's about consistently delivering an unparalleled experience with every sip.</p>
                 <a href="" class="btn">Explore Now &#10172;</a>
-
             </div>
-            <div= class="col-2">
-                <img src="image2.jpg">
+            <div class="col-2">
+                <img src="image3.jpg">
+            </div>
         </div>
     </div>
-</body>
 
-<!-------------------footer-------------------->
-<div class="footer">
-    <p>&copy; 2024 Cup Corner. All rights reserved.</p>
-</div>
+    <!-- Footer -->
+    <div class="footer">
+        <p>&copy; 2024 Cup Corner. All rights reserved.</p>
+    </div>
+</body>
 </html>
