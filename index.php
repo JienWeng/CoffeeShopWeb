3486aff816c1696f8d001c4a106e8f08df5d4b29
Janzy16 <janiceee456@gmail.com>
2024-03-17 20:59:21 +0800

    homepage 2.0

--- index.php
+++ index.php
@@ -1,49 +1,238 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
     <title>Cup Corner</title>
     <link rel="stylesheet" href="style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
+    
     <div class="header">
-        <div class="container">
-            <div class="navbar">
-                <div class="logo">
-                    <a href="index.html"><img src="cupcorner-logo.jpg" width="125px"></a>
+    <div class="container">
+        <div class="navbar">
+         <div class="logo">
+            <img src="cupcorner-logo.jpg" width="125px">
+         </div>
+         <nav>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Menu</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Contact</a></li>
+                <li><a href="">Account</a></li>
+            </ul>
+         </nav>
+         <img src="image4.png" width = "30px" height="30px">
+    </div>
+    <div class="row">
+        <div class="col-2">
+            <h1>Embrace the Aroma: <br>Elevate Your Coffee Experience Today!</h1>
+            <p>Savoring a perfect cup of coffee isn't just about that initial burst of flavor,<br>it's about consistently delivering an unparalleled experience with every sip.</p>
+            <a href="" class="btn">Explore Now &#10172;</a>
+        </div>
+        <div class="col-2">
+            <img src="1stpg.png" width="425px">
+        </div>
+    </div>
+   </div>
+</div>
+    
+<!----------featured categories---------->
+    <div class="categories">
+        <div class="small-container">
+            <div class="row">
+             <div class="col-3">
+                <img src="ad.coffee1.png">
+             </div>
+             <div class="col-3">
+                 <img src="ad.coffee3.png">
+             </div>
+             <div class="col-3">
+                 <img src="ad.coffee2.png">
+             </div>
+         </div>
+        </div>
+    </div>
+
+<!----------featured menu---------->
+    <div class="small-container">
+        <h2 class="title">Seasonal Flavors</h2>
+        <div class="row">
+            <div class="col-4">
+                <img src="new.coffee.png">
+                <h4>Pumpkin Spice Latte</h4>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-half"></i>
                 </div>
-                <nav>
-                    <ul>
-                        <li><a href="">Home</a></li>
-                        <li><a href="">Menu</a></li>
-                        <li><a href="">About</a></li>
-                        <li><a href="">Contact</a></li>
-                        <li><a href="">Account</a></li>
-                        <li><a href="">Cart</a></li>
-                    </ul>
-                </nav>
+                <p>RM14.90</p>
+            </div>
+            <div class="col-4">
+                <img src="new.cake.png">
+                <h4>Cranberry Orange Cake</h4>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                </div>
+                <p>RM18.90</p>
+            </div>
+            <div class="col-4">
+                <img src="new.food.jpg">
+                <h4>Maple Pecan Scones</h4>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-o"></i>
+                </div>
+                <p>RM9.90</p>
+            </div>
+            <div class="col-4">
+                <img src="new.coffee2.png">
+                <h4>Caramel Apple Latte</h4>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                </div>
+                <p>RM15.90</p>
+            </div>
+        </div>
+        <h2 class="title">All Time Favourites</h2>
+        <div class="row">
+            <div class="col-4">
+                <img src="atf.coffee.png">
+                <h4>Cupa Cozy</h4>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                </div>
+                <p>RM11.90</p>
+            </div>
+            <div class="col-4">
+                <img src="atf.cake.png">
+                <h4>Tiramisu Cake</h4>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                </div>
+                <p>RM12.90</p>
+            </div>
+            <div class="col-4">
+                <img src="atf.coffee2.png">
+                <h4>Americano</h4>
+                <div class="rating">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                </div>
+                <p>RM7.90</p>
             </div>
         </div>
     </div>
-    <div class="container">
-        <div class="row">
-            <div class="col-2">
-                <h1>Embrace the Aroma: <br>Elevate Your Coffee Experience Today!</h1>
-                <p>Savoring a perfect cup of coffee isn't just about that initial burst of flavor,<br>it's about consistently delivering an unparalleled experience with every sip.</p>
-                <a href="" class="btn">Explore Now &#10172;</a>
-            </div>
-            <div class="col-2">
-                <img src="image3.jpg">
+<!----------offer---------->
+    <div class="offer">
+        <div class="small container">
+            <div class="row">
+                <div class="col-2">
+                    <img src="limited.ed.png" class="limited-img">
+                </div>
+                <div class="col-2">
+                    <p>Spooktacular saving</p>
+                    <h1> Pumpkin Patch Pals</h1>
+                    <small>Add some festive flair to your Halloween celebrations with our adorable Pumpkin Patch Pals plush! This cuddly companion is the perfect addition to your spooky decor, featuring soft, plush fabric and charming pumpkin design. Receive this delightful plush as a free gift with the purchase of any of our seasonal drinks, and bring a touch of whimsy to your festivities. Hurry, while supplies last! </small>
+                </div>
             </div>
         </div>
     </div>
 
-    <!-- Footer -->
-    <div class="footer">
-        <p>&copy; 2024 Cup Corner. All rights reserved.</p>
+<!----------customer review---------->
+    <div class="cusreview">
+        <div class="small-container">
+            <div class="row">
+                <div class="col-3">
+                    <i class="fa fa-quote-left"></i>
+                     <p>As a Muslim, I truly appreciate Cup Corner's unwavering commitment to offering Halal-certified coffee. Their rich blends never fail to uplift my mornings with their smooth and aromatic flavors.</p>
+                    <div class = "rating">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star-half"></i>
+                    </div>
+                    <img src="cus.rev.png">
+                    <h3>Nur Fazira, Malaysian Actress</h3>
+                </div>
+                <div class="col-3">
+                    <i class="fa fa-quote-left"></i>
+                     <p>Cup Corner's coffee is a delightful blend of tradition and innovation. I appreciate the balance of bold flavors and subtle nuances in every cup, making it a perfect companion for any occasion.</p>
+                    <div class = "rating">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star-o"></i>
+                    </div>
+                    <img src="cus.rev2.png">
+                    <h3>Chong Yu Tou, Utar professor</h3>
+                </div>
+                <div class="col-3">
+                    <i class="fa fa-quote-left"></i>
+                     <p>Cup Corner's coffee transports me back to the bustling streets of India, where coffee is more than just a drink – it's a ritual. Their robust brews provide the perfect pick-me-up during hectic days.</p>
+                    <div class = "rating">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                    </div>
+                    <img src="cus.rev3.png">
+                    <h3>Kumar A/L Aditya, CEO of Microhard </h3>
+                </div>
+            </div>
+        </div>
     </div>
+<!----------brand---------->
+    <div class="brands">
+        <div class="small-container">
+            <div class="row">
+                <div class="col-5">
+                    <img src="blogo1.png" width="125px">
+                </div>
+                <div class="col-5">
+                    <img src="blogo2.png" width="125px">
+                </div>
+                <div class="col-5">
+                    <img src="blogo3.png" width="125px">
+                </div>
+                <div class="col-5">
+                    <img src="blogo4.png" width="125px">
+                </div>
+                
+            </div>
+        </div>
+    </div>
+    
+    
+    
 </body>
 </html>
