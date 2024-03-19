600604c9a75c4e9c0db2f03f9f51f67abe5f5bbd
Janzy16 <janiceee456@gmail.com>
2024-03-19 12:41:07 +0800

    contact category

---
+++ contact.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Us</title>
+    <link rel="stylesheet" href="contact.css"> 
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+</head>
+<body>
+
+<section class="contact">
+    <div class="content">
+        <h2>Contact Us</h2>
+        <p>Have a question or feedback? Feel free to reach out to us - we'd love to hear from you! Contact Cup Corner today for any inquiries or assistance.</p>
+    </div>
+    <div class="container">
+        <div class="contactInfo">
+            <div class="box">
+                <div class="icon"><i class="fa fa-map-marker"></i></div>
+                <h3>Address</h3>
+                <p>1, Jalan Sungai Long,<br>43200 Kajang, Selangor</p>
+            </div>
+            <div class="box">
+                <div class="icon"><i class="fa fa-phone"></i></div>
+                <h3>Phone</h3>
+                <p>012-1232094</p>
+            </div>
+            <div class="box">
+                <div class="icon"><i class="fa fa-envelope"></i></div>
+                <h3>Email</h3> <p>cupcorner@gmail.com</p>
+            </div>
+        </div>
+    </div>
+    <div class="contactform">
+        <form>
+            <h2>Feedback</h2>
+            <div class="inputBox">
+                <input type="text" name="" required="required">
+                <span>Full Name</span>
+            </div>
+            <div class="inputBox">
+                <input type="text" name="" required="required">
+                <span>Email</span>
+            </div>
+            <div class="inputBox">
+                <textarea required="required"></textarea>
+                <span>Type your Message...</span>
+            </div>
+            <div class="inputBox">
+                <input type="submit" name="" value="send">
+            </div>
+        </form>
+    </div>
+</section>
+
+</body>
+</html>
