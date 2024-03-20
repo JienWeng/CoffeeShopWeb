456fbb9685d14f053c29b54a9b83e99177bca291
Jidannnn <reallyhat@gmail.com>
2024-03-20 22:54:47 +0800

    added menu + admin function

--- menu.php
+++ menu.php
@@ -6,69 +6,46 @@
     <title>Cup Coffee Menu</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link rel="stylesheet" href="menu.css">
-
+    <script src='menu.js'></script>
 </head>
 <body>
-    <!---------navbar----------->
-<?php include("include/navbar.php") ?>
 
-    <!-----------contents---------->
-<div class="row">
+<!-----include the conenct php as $con------>
+<?php 
+include('./include/connect.php'); 
+include('./functions/common_function.php');
+?>
+
+<!-----------contents---------->
+<div class="row products-container">
     <div class="col-md-10">
 <!-----------products---------->
         <div class="row">
-            <div class="col-md-4">
-                <div class="card" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Product 1</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Add to Cart</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Product 2</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Add to Cart</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Product 3</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Add to Cart</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Product 3</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Add to Cart</a>
-                    </div>
-                </div>
-            </div>
+<!----------fetching products--------->
+        <?php 
+        getProducts();
+        getUniqueCategory();
+        ?>
         </div>
-
     </div>
-    <div class="col-md-2">
+    <div class="side-nav col-md-2 bg-secondary">
 <!-----------side nav---------->
-    <h1>yes</h1>
-    <h1>hello</h1>
-    <h1>hello</h1>
-    <h1>hello</h1>
-    <h1>hello</h1>
+        <ul class="navbar-nav me-auto">
+            <li class="nav-item bg-info">
+                <a href="#" class="nav-link"><h4>Categories</h4></a>
+            </li>
+            <?php
+            getCategories();
+            ?>
+        </ul>
     </div>
-</div>
+
+    <div class='products-preview'>
+        <?php 
+            getProductPreview();
+        ?>
+    </div>
 </body>
 </html>
