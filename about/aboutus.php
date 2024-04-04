f80afab4f7f3a9332d59a82cc4e0abb42db45f64
Janzy16 <janiceee456@gmail.com>
2024-04-04 17:01:30 +0800

    about us
    draft only

---
+++ about/aboutus.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <title>About Us</title>
+    <link rel="stylesheet" type="text/css" href="aboutus.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    
+    
+</head>    
+    
+<body>
+
+    <section class="home" id="home">
+        <div class="home-content">
+            <h1>Hi, It's <span>Cup Corner Team</span></h1>
+            <h3 class="text-animation">We are <span></span></h3>
+            <p>Cup Corner Team is founded by a group of coffee enthusiasts and has grown from humble beginnings to become a beloved destination for coffee lovers all over the world. Our journey began with a simple desire: to create a space where the rich aroma of freshly brewed coffee and the warmth of community could coexist seamlessly. Cup Corner stands today as a testament to our passion for coffee, serving a diverse selection of meticulously sourced and expertly crafted beverages. At the heart of our brand is a dedication to quality and a strong belief in building relationships over coffee. Join us at Cup Corner, where each sip reveals a story of passion and excellence.</p>
+            
+            <div class="social-icons">
+                <a href="#"><i class='bx bxl-linkedin'></i></a>
+                <a href="#"><i class='bx bxl-github'></i></a>
+                <a href="#"><i class='bx bxl-instagram-alt' ></i></a>
+                <a href="#"><i class='bx bxl-twitter' ></i></a>
+            </div>
+            <div class="btn-group">
+                <a href="#" class="btn"> Hire</a>
+                <a href="#Contact" class="btn">Contact</a>
+            </div>
+        </div>
+            <div class="home-img">
+                <img src="about%20us.png" alt=" ">
+            </div>
+        
+    </section>
+</body>
+</html>
